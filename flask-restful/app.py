a2303e69dc399e53ffb4579fa0dcf768b7def858
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-06-21 01:30:04 +0200

    🐍 flask-restful example

--- flask-restful/app.py
+++ flask-restful/app.py
@@ -5,13 +5,16 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
 @app.route("/")
 def hello_world():
     return "<h1>Hello, World!</h1>"
 
+
 class User(Resource):
     def get(self):
         return "hello world"
+
 
 api.add_resource(User, "/users")
 
