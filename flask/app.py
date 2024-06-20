e8e53438a40da7d67b561be6b6e81b1ebb8a00d3
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-06-21 00:02:18 +0200

    🐍 flask example

---
+++ flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask.cli import main
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+if __name__ == "__main__":
+    main()
