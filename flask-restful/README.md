14f8abb2a33c28a83362be23c6d2abe4aab2a4c1
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-06-21 00:54:37 +0200

    🐍 flask-restful example

---
+++ flask-restful/README.md
@@ -0,0 +1,3 @@
+* https://flask.palletsprojects.com/en/latest/quickstart/
+* https://flask-restful.readthedocs.io/en/latest/quickstart.html
+* https://www.youtube.com/watch?v=GMppyAPbLYk
---
+++ flask-restful/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask.cli import main
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello, World!</h1>"
+
+class User(Resource):
+    def get(self):
+        return "hello world"
+
+api.add_resource(User, "/users")
+
+if __name__ == "__main__":
+    main()
